f8f9b6c09e6c2d3efab0b897cc5ba8d29834568b
wasja1982 <wasja@yandex.ru>
2014-09-19 08:25:50 +0300

    Функционал вынесен в отдельный класс.

--- PluginBlacklist.class.php
+++ PluginBlacklist.class.php
@@ -36,87 +36,9 @@ class PluginBlacklist extends Plugin {
     public function Init() {
     }
 
-    static function check_whitelist_domains($sMail) {
-        $aMail = explode("@", $sMail);
-        $sDomain = (count($aMail) > 1 ? $aMail[1] : '');
-        return in_array(strtolower($sDomain), Config::Get('plugin.blacklist.whitelist_domains'));
-    }
-
-    static function check_blacklist_domains($sMail) {
-        $aMail = explode("@", $sMail);
-        $sDomain = (count($aMail) > 1 ? $aMail[1] : '');
-        return in_array(strtolower($sDomain), Config::Get('plugin.blacklist.blacklist_domains'));
-    }
-
-    static function check_stopforumspam_org($sMail) {
-        $aParams = array(
-            'f' => 'json',
-            'email' => $sMail,
-        );
-        $sUrl = 'http://api.stopforumspam.org/api' . '?' . urldecode(http_build_query($aParams));
-        $sAnswer = @file_get_contents($sUrl);
-        $aInfo = json_decode($sAnswer, true);
-        if (isset($aInfo['success']) && $aInfo['success']) {
-            if (isset($aInfo['email']) && isset($aInfo['email']['appears'])) {
-                return $aInfo['email']['appears'];
-            }
-        }
-        return false;
-    }
-
-    static function check_botscout_com($sMail) {
-        $aParams = array(
-            'key' => Config::Get('plugin.blacklist.key_botscout_com'),
-            'mail' => $sMail,
-        );
-        $sUrl = 'http://botscout.com/test/' . '?' . urldecode(http_build_query($aParams));
-        $sAnswer = @file_get_contents($sUrl);
-        if ($sAnswer && substr($sAnswer, 0, 1) === 'Y') {
-            return true;
-        }
-        return false;
-    }
-
-    static function check_fspamlist_com($sMail) {
-        $aParams = array(
-            'json' => true,
-            'key' => Config::Get('plugin.blacklist.key_fspamlist_com'),
-            'spammer' => $sMail,
-        );
-        $sUrl = 'http://www.fspamlist.com/api.php' . '?' . urldecode(http_build_query($aParams));
-        $sAnswer = @file_get_contents($sUrl);
-        $aInfo = json_decode($sAnswer, true);
-        if (count($aInfo)) {
-            foreach ($aInfo as $aItem) {
-                if (isset($aItem['isspammer'])) {
-                    return $aItem['isspammer'];
-                }
-            }
-        }
-        return false;
-    }
-
     static function blackMail($sMail) {
-        if (empty($sMail)) {
-            return false;
-        }
-        if (PluginBlacklist::check_whitelist_domains($sMail)) {
-            return false;
-        }
-        if (PluginBlacklist::check_blacklist_domains($sMail)) {
-            return true;
-        }
-        $bResult = false;
-        if (Config::Get('plugin.blacklist.use_stopforumspam_org')) {
-            $bResult = PluginBlacklist::check_stopforumspam_org($sMail);
-        }
-        if (!$bResult && Config::Get('plugin.blacklist.use_botscout_com')) {
-            $bResult = PluginBlacklist::check_botscout_com($sMail);
-        }
-        if (!$bResult && Config::Get('plugin.blacklist.use_fspamlist_com')) {
-            $bResult = PluginBlacklist::check_fspamlist_com($sMail);
-        }
-        return $bResult;
+        $oEngine = Engine::getInstance ();
+        return $oEngine -> PluginBlacklist_ModuleBlacklist_blackMail($sMail);
     }
 }
 ?>
