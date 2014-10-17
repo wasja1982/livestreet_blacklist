d0fc48e151bdf6a60fe66517763e1d521dd4ccb7
wasja1982 <wasja@yandex.ru>
2014-10-17 08:12:49 +0300

    Версия 1.0.2

--- templates/language/russian.php
+++ templates/language/russian.php
@@ -2,7 +2,7 @@
 /**
  * Blacklist - проверка E-Mail пользователей на наличие в базах спамеров.
  *
- * Версия:	1.0.1
+ * Версия:	1.0.2
  * Автор:	Александр Вереник
  * Профиль:	http://livestreet.ru/profile/Wasja/
  * GitHub:	https://github.com/wasja1982/livestreet_blacklist
